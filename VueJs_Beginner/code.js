eaaf10eb919bf79f6bbdff8839bd9ab3e7b24434
Wannn Sion <wannnsion95@gmail.com>
2025-08-04 16:46:48 +0700

    Add files via upload

---
+++ VueJs_Beginner/code.js
@@ -0,0 +1,8 @@
+new Vue({
+  el: "#apk",
+  data: {
+    name: "",
+    pesan:
+      "Aku sangat mencintaimu, tapi aku terlalu takut untuk mengungkapkannya kepadamu, karena aku masih merasa tidak pantas untuk bersanding denganmu.",
+  },
+});
